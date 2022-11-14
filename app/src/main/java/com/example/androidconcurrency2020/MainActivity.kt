998f263b23e0cd3ca9fff42cfff2758af980c3b1
callmatos <callmatos@gmail.com>
2022-11-14 10:04:26 -0400

    Challenge dados

--- app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.example.androidconcurrency2020
 
-import android.os.*
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
-import android.widget.ScrollView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidconcurrency2020.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
+import kotlin.random.Random
 
-const val MESSAGE_KEY = "mss_key"
+const val DIE_INDEX_KEY = "die_index_key"
+const val DIE_VALUE_KEY = "die_value_key"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var imageViews: Array<ImageView>
+
+    private val drawables = arrayOf(
+        R.drawable.die_1, R.drawable.die_2,
+        R.drawable.die_3, R.drawable.die_4,
+        R.drawable.die_5, R.drawable.die_6
+    )
+
     private val handler = object : Handler(Looper.getMainLooper()){
         override fun handleMessage(msg: Message) {
             val bundle = msg.data
-            val message = bundle?.getString(MESSAGE_KEY)
-            logEditTextView(message ?: "message was null")
+            val dieIndex = bundle?.getInt(DIE_INDEX_KEY) ?: 0
+            val dieValue = bundle?.getInt(DIE_VALUE_KEY) ?: 1
+            Log.i(LOG_TAG, "index=$dieIndex,value=$dieValue")
+            imageViews[dieIndex].setImageResource(drawables[dieValue - 1])
         }
     }
 
@@ -28,61 +43,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initialize button click handlers
-        with(binding) {
-            runButton.setOnClickListener { runCode() }
-            clearButton.setOnClickListener { clearOutput() }
-        }
+        imageViews = arrayOf(binding.die1,binding.die2,binding.die3,binding.die4,binding.die5)
+        binding.rollButton.setOnClickListener { rollTheDice() }
 
     }
 
     /**
      * Run some code
      */
-    private fun runCode() {
+    private fun rollTheDice() {
 
-    thread(start = true){
-        val bundle = Bundle()
-        for (i in 1..10){
-            bundle.putString(MESSAGE_KEY,"Looping $1")
-            Message().also {
-                it.data = bundle
-                handler.sendMessage(it)
+        for (dieIndex in imageViews.indices) {
+
+            thread(start = true){
+                Log.i(LOG_TAG, "Thread init $dieIndex")
+                Thread.sleep(dieIndex * 10L)
+                val bundler = Bundle()
+                bundler.putInt(DIE_INDEX_KEY, dieIndex)
+
+                for (i in 1..20){
+                    bundler.putInt(DIE_VALUE_KEY,getDieValue())
+                    Message().also {
+                        it.data = bundler
+                        handler.sendMessage(it)
+                    }
+                    Thread.sleep(100)
+                }
+                Log.i(LOG_TAG, "Thread end $dieIndex")
             }
-            Thread.sleep(1000)
-        }
-        bundle.putString(MESSAGE_KEY,"All done")
-        Message().also {
-            it.data = bundle
-            handler.sendMessage(it)
         }
     }
 
-    }
-
-    /**bundle
-     * Clear log display
-     */
-    private fun clearOutput() {
-        binding.logDisplay.text = ""
-        scrollTextToEnd()
-    }
-
     /**
-     * Log output to logcat and the screen
+     * Get a random number from 1 to 6
      */
-    @Suppress("SameParameterValue")
-    private fun logEditTextView(message: String) {
-        Log.i(LOG_TAG, message)
-        binding.logDisplay.append(message + "\n")
-        scrollTextToEnd()
-    }
-
-    /**
-     * Scroll to end. Wrapped in post() function so it's the last thing to happen
-     */
-    private fun scrollTextToEnd() {
-        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    private fun getDieValue(): Int {
+        return Random.nextInt(1, 7)
     }
 
 }
